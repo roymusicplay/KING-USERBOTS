f235548d50637f8914286c3fbe7b02f54b349d7a
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-05-18 17:12:36 +0530

    Update unmute.py

--- adminss/unmute.py
+++ adminss/unmute.py
@@ -11,7 +11,7 @@ __**This command helps you to unmute a user in the chat**__
 """
 
 @kingbot.on_message(filters.group & filters.command("unmute",vr.get("HNDLR")) & filters.user(Adminsettings))  
-def unmute(_, message):
+async def unmute(_, message):
     msg_id=message.message_id
     chat_msg=message.text
     username=None
@@ -24,18 +24,18 @@ def unmute(_, message):
         username=message.reply_to_message.from_user.id
 
     chat_id=message.chat.id
-    can_unmute=kingbot.get_chat_member(chat_id , "me").can_restrict_members
-    user_info=kingbot.get_users(username)
+    can_unmute=await kingbot.get_chat_member(chat_id , "me").can_restrict_members
+    user_info=await kingbot.get_users(username)
     if(can_unmute):      
-        kingbot.restrict_chat_member(chat_id , username , message.chat.permissions)
+        await kingbot.restrict_chat_member(chat_id , username , message.chat.permissions)
         if(user_info.username):
             usercontact=user_info.username
             reply_string="@"+usercontact+" has been released 😈"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
         else:
             usercontact=user_info.first_name
             reply_string=usercontact+" has been released 😈"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
     else:
         reply_string="Noob,you can't unmute members 😂 !"
-        kingbot.edit_message_text(chat_id , msg_id , reply_string )
+        await kingbot.edit_message_text(chat_id , msg_id , reply_string )
