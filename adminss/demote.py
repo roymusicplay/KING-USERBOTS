29aeee4670eeae0a29bc362a5191e21ba4de371a
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-05-18 17:07:18 +0530

    Update demote.py

--- adminss/demote.py
+++ adminss/demote.py
@@ -11,12 +11,12 @@ __**This command helps you to instantly demote someone in the chat**__
 """
 
 @kingbot.on_message(filters.group & filters.command("demote",vr.get("HNDLR")) & filters.user(Adminsettings))  
-def demote(_, message):
+async def demote(_, message):
     msg_id=message.message_id
     user_id=message.reply_to_message.from_user.id
     chat_id=message.chat.id
-    can_promote=kingbot.get_chat_member(chat_id , "me").can_promote_members
-    user_info=kingbot.get_users(user_id)
+    can_promote=await kingbot.get_chat_member(chat_id , "me").can_promote_members
+    user_info=await kingbot.get_users(user_id)
     usercontact=None
     if(can_promote):
         chat_msg=message.text
@@ -25,7 +25,7 @@ def demote(_, message):
             space=chat_msg.index(" ")     
             chat_msg=str(chat_msg)
             title=chat_msg[space+1:len(chat_msg)]
-        kingbot.promote_chat_member(
+        await kingbot.promote_chat_member(
             chat_id,
             user_id,
             can_manage_chat=False,
@@ -41,21 +41,21 @@ def demote(_, message):
             can_promote_members=False
         )
         if(title):
-            kingbot.set_administrator_title(chat_id, user_id,title)
+            await kingbot.set_administrator_title(chat_id, user_id,title)
         if(user_info.username):
             usercontact=user_info.username
             reply_string="@"+usercontact+" has been demoted due to non-payment"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
         else:
             usercontact=user_info.first_name
             reply_string=usercontact+" has been demoted due to non-payment"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
     else:
         reply_string="Noob,you are not an admin !"
         reply_len=len(reply_string)
         for i in range(reply_len):
             edit_string=reply_string[0:i+2]
             try:
-                kingbot.edit_message_text(chat_id , msg_id , edit_string )
+                await kingbot.edit_message_text(chat_id , msg_id , edit_string )
             except:
                 i=i+1
