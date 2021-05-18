a92c8111eb487fc7a85dbf3cb6c05e831b2408cf
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-05-18 17:17:21 +0530

    Update setchatphoto.py

--- adminss/setchatphoto.py
+++ adminss/setchatphoto.py
@@ -11,16 +11,16 @@ __**This command helps you set chat photo **__
 """
 
 @kingbot.on_message(filters.group & filters.command("setchatphoto",vr.get("HNDLR")) & filters.user(Adminsettings))  
-def set_chat_photo(_, message):
+async def set_chat_photo(_, message):
     msg_id=message.message_id
     chat_id=message.chat.id
-    can_change_admin=kingbot.get_chat_member(chat_id , "me").can_change_info
+    can_change_admin=await kingbot.get_chat_member(chat_id , "me").can_change_info
     can_change_member=message.chat.permissions.can_change_info
     if not (can_change_admin or can_change_member):
-        message.reply("You don't have enough permission")
+        await message.edit_text("You don't have enough permission")
     if message.reply_to_message:
         if message.reply_to_message.photo:
-            kingbot.set_chat_photo(chat_id , photo=message.reply_to_message.photo.file_id)
+            await kingbot.set_chat_photo(chat_id , photo=message.reply_to_message.photo.file_id)
             return
     else:
         message.reply("Reply to a photo to set it !")
