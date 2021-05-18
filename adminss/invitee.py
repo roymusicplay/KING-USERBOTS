17c4b682ed4df102821906885e0eed3451f41fa1
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-05-18 17:35:22 +0530

    Create invitee.py

---
+++ adminss/invitee.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from kingbot import kingbot, setbot , vr , Adminsettings
+import asyncio
+__MODULE__ = "Spam"
+__HELP__ = """
+->`spam`
+__**This command helps you to spam in a chat use the format spam times text**__
+──「 **Usage** 」──
+"""
+@kingbot.on_message(filters.user(Adminsettings) & filters.command("invite", vr.get("HNDLR")))
+async def inviteee(client, message):
+ mg = await message.edit_text("`Adding Users!`")
+    user_s_to_add = message.text.split(" ",1)[1]
+    if not user_s_to_add:
+        await mg.edit("`Give Me Users To Add! Check Help Menu For More Info!`")
+        return
+    user_list = user_s_to_add.split(" ")
+    try:
+        await client.add_chat_members(message.chat.id, user_list, forward_limit=100)
+    except BaseException as e:
+        await mg.edit(f"`Unable To Add Users! \nTraceBack : {e}`")
+        return
+    await mg.edit(f"`Sucessfully Added {len(user_list)} To This Group / Channel!`")
