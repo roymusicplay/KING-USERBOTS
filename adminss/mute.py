3b1a159ca526829d9929364e34435e9e5fd8ad99
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-05-18 17:08:51 +0530

    Update mute.py

--- adminss/mute.py
+++ adminss/mute.py
@@ -11,7 +11,7 @@ __**This command helps you to mute a user in the chat**__
 """
 
 @kingbot.on_message(filters.command("mute",vr.get("HNDLR")) & filters.user(Adminsettings))  
-def mute(_, message):
+async def mute(_, message):
     
     msg_id=message.message_id
     chat_id=message.chat.id
@@ -25,25 +25,25 @@ def mute(_, message):
         user_id=chat_msg[index+1:len(chat_msg)]
     else:                   
         user_id=message.reply_to_message.from_user.id
-    user_info=kingbot.get_users(user_id)
-    can_user_ban=kingbot.get_chat_member(chat_id , user_id).can_restrict_members
+    user_info=await kingbot.get_users(user_id)
+    can_user_ban=await kingbot.get_chat_member(chat_id , user_id).can_restrict_members
 
     if(can_mute):
         if(can_user_ban):
             reply_string="Can't mute another admin. LOL !"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string )
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string )
         else:            
 
-            kingbot.restrict_chat_member(chat_id , user_id , ChatPermissions() , int(time()+86400))
+            await kingbot.restrict_chat_member(chat_id , user_id , ChatPermissions() , int(time()+86400))
             if(user_info.username):
                 usercontact=user_info.username
                 reply_string="@"+usercontact+" has been muted for 24hrs !"
-                kingbot.edit_message_text(chat_id , msg_id , reply_string)
+                await kingbot.edit_message_text(chat_id , msg_id , reply_string)
             else:
                 usercontact=user_info.first_name
                 reply_string=usercontact+" has been muted for 24hrs !"
-                kingbot.edit_message_text(chat_id , msg_id , reply_string)
+                await kingbot.edit_message_text(chat_id , msg_id , reply_string)
     else:
         reply_string="Noob,you can't mute members 😂 !"
-        kingbot.edit_message_text(chat_id , msg_id , reply_string )
+        await kingbot.edit_message_text(chat_id , msg_id , reply_string )
 
