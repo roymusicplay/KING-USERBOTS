6207c3475ea738da7a0be52dafcd8ecf9fa7ef0a
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-08 06:26:29 +0530

    Update paste.py

--- utilss/paste.py
+++ utilss/paste.py
@@ -9,28 +9,41 @@ __**This command helps you to paste a text to nekobin.com and return a shareable
 ──「 **Usage** 」──
 -> `paste`
 """
-
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 @kingbot.on_message(filters.command("paste",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def paste(_,message):
-   msg_txt=message.text
-   if msg_txt:
-    if " " in msg_txt:
-        content=msg_txt[msg_txt.index(" ")+1:len(msg_txt)]
-        req = requests.post('https://nekobin.com./api/documents',json={"content":content})
-        key = json.loads(req.content)["result"]["key"]
-        await message.reply(f"Nekofied to https://nekobin.com/{key}")
-    else:
-        if message.reply_to_message:
-            if message.reply_to_message.text:
-                content=message.reply_to_message.text
-                req = requests.post('https://nekobin.com./api/documents',json={"content":content})
-                key = json.loads(req.content)["result"]["key"]
-                await message.reply(f"Nekofied to https://nekobin.com/{key}")
-   elif not message.reply_to_message.text:
+    rep = await message.edit_text("`Please Wait.....`")
+    tex_t = get_text(message)
+    message_s = tex_t
+    if not tex_t:
+        if not message.reply_to_message:
+            await pablo.edit("`Reply To File / Give Me Text To Paste!`")
+            return
+        if not message.reply_to_message.text:
             file = await message.reply_to_message.download()
             m_list = open(file, "r").read()
-            content = m_list
+            message_s = m_list
             os.remove(file)
-            req = requests.post('https://nekobin.com./api/documents',json={"content":content})
-            key = json.loads(req.content)["result"]["key"]
-            await message.reply(f"Nekofied to https://nekobin.com/{key}")
+        elif message.reply_to_message.text:
+            message_s = message.reply_to_message.text
+    key = (
+        requests.post("https://nekobin.com/api/documents", json={"content": message_s})
+        .json()
+        .get("result")
+        .get("key")
+    )
+    url = f"https://nekobin.com/{key}"
+    raw = f"https://nekobin.com/raw/{key}"
+    reply_text = f"Pasted Text To [NekoBin]({url}) And For Raw [Click Here]({raw})"
+    await rep.edit(reply_text)
