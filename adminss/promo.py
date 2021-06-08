75325064a0cb0c69f3524602949090d9fafa41a2
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-08 15:56:45 +0530

    Update promo.py

--- adminss/promo.py
+++ adminss/promo.py
@@ -15,8 +15,9 @@ async def promotte(_, message):
     msg_id=message.message_id
     user_id=message.reply_to_message.from_user.id
     chat_id=message.chat.id
-    can_promote=kingbot.get_chat_member(chat_id , message.user.id).can_promote_members
-    user_info=kingbot.get_users(user_id)
+    zuzu= await kingbot.get_chat_member(chat_id , message.user.id)
+    can_promote=zuzu.can_promote_members
+    user_info=await kingbot.get_users(user_id)
     usercontact=None
     if(user_info.username):
         usercontact=user_info.username
@@ -30,7 +31,7 @@ async def promotte(_, message):
             space=chat_msg.index(" ")     
             chat_msg=str(chat_msg)
             title=chat_msg[space+1:len(chat_msg)]
-        app.promote_chat_member(
+        await kingbot.promote_chat_member(
             chat_id,
             user_id,
             can_manage_chat=True,
@@ -41,21 +42,21 @@ async def promotte(_, message):
             can_invite_users=True
         )
         if(title):
-            kingbot.set_administrator_title(chat_id, user_id,title)
+            await kingbot.set_administrator_title(chat_id, user_id,title)
         if(user_info.username):
             usercontact=user_info.username
             reply_string="@"+usercontact+" has been promoted due to bribe"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
         else:
             usercontact=user_info.first_name
             reply_string=usercontact+" has been promoted due to bribe"
-            kingbot.edit_message_text(chat_id , msg_id , reply_string)
+            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
     else:
         reply_string="Noob,you are not an admin !"
         reply_len=len(reply_string)
         for i in range(reply_len):
             edit_string=reply_string[0:i+2]
             try:
-                kingbot.edit_message_text(chat_id , msg_id , edit_string )
+                await kingbot.edit_message_text(chat_id , msg_id , edit_string )
             except:
                 i=i+1
