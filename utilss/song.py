368561ff2850fa7b39cdbc075a77eeb2dd11cedf
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-08 10:23:37 +0530

    Update song.py

--- utilss/song.py
+++ utilss/song.py
@@ -108,7 +108,7 @@ async def savnana(client, message):
     urlretrieve(urrl, title + ".mp3")
     urlretrieve(img, title + ".jpg")
     file= await wget.download(urrl)
-    await client.send_audio(message.chat.id,file,caption=f"Song from saavan uploaded by king Userbot \n Song name={title}\n Singers={singers}) )   
+    await client.send_audio(message.chat.id,file,caption=f"Song from saavan uploaded by king Userbot \n Song name={title}\n Singers={singers}" )   
     await lol.delete()
     os.remove(title + ".mp3")
     os.remove(title + ".jpg")
