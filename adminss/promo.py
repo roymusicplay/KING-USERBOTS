6f10ebefde3355c81bd4768afb6566bb97da738e
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-11 14:36:54 +0530

    Update promo.py

--- adminss/promo.py
+++ adminss/promo.py
@@ -1,7 +1,16 @@
 from pyrogram import client, filters
 import asyncio
 import time
+from emoji import get_emoji_regexp
 from pyrogram.types import ChatPermissions
+from pyrogram.errors import (
+    UsernameInvalid,
+    ChatAdminRequired,
+    PeerIdInvalid,
+    UserIdInvalid,
+    UserAdminInvalid,
+    FloodWait,
+)
 from kingbot import kingbot, vr ,Adminsettings
 __MODULE__ = "promote"
 __HELP__ = """
@@ -12,54 +21,74 @@ __**This command helps you to instantly promote someone in the chat**__
 
 @kingbot.on_message(filters.group & filters.command("promote",vr.get("HNDLR")) & filters.user(Adminsettings))  
 async def promotte(_, message):
-    msg_id=message.message_id
-    user_id=message.reply_to_message.from_user
-    user_id = user_id.id
-    chat_id=message.chat.id
-    zuzuzu= message.from_user
-    zuzu= await kingbot.get_chat_member(chat_id , zuzuzu.id)
-    print(zuzu)
-    can_promote=zuzu.can_promote_members
-    user_info=await kingbot.get_users(user_id)
-    usercontact=None
-    if(user_info.username):
-        usercontact=user_info.username
-    else:
-        usercontact=user_info.first_name
-
-    if can_promote:
-        chat_msg=message.text
-        title=None
-        if " " in chat_msg:
-            space=chat_msg.index(" ")     
-            chat_msg=str(chat_msg)
-            title=chat_msg[space+1:len(chat_msg)]
-        await kingbot.promote_chat_member(
-            chat_id,
-            user_id,
-            can_manage_chat=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_pin_messages=True,
-            can_manage_voice_chats=True,
-            can_invite_users=True
-        )
-        if(title):
-            await kingbot.set_administrator_title(chat_id, user_id,title)
-        if(user_info.username):
-            usercontact=user_info.username
-            reply_string="@"+usercontact+" has been promoted due to bribe"
-            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
-        else:
-            usercontact=user_info.first_name
-            reply_string=usercontact+" has been promoted due to bribe"
-            await kingbot.edit_message_text(chat_id , msg_id , reply_string)
-    else:
-        reply_string="Noob,you are not an admin !"
-        reply_len=len(reply_string)
-        for i in range(reply_len):
-            edit_string=reply_string[0:i+2]
+    if message.chat.type in ["group", "supergroup"]:
+        cmd = message.command
+    me_ = await message.chat.get_member(int(me_m.id))
+    if not me_.can_promote_members:
+        await message.edit("`Boss, You Don't Have Promote Permission!`")
+        return
+        can_promo = True
+        if can_promo:
             try:
-                await kingbot.edit_message_text(chat_id , msg_id , edit_string )
-            except:
-                i=i+1
+                if message.reply_to_message:
+                    user_id = message.reply_to_message.from_user.id
+                    custom_rank = get_emoji_regexp().sub("", " ".join(cmd[1:]))
+                else:
+                    usr = await client.get_users(cmd[1])
+                    custom_rank = get_emoji_regexp().sub("", " ".join(cmd[2:]))
+                    user_id = usr.id
+            except IndexError:
+                await message.delete()
+                return
+
+            if user_id:
+                try:
+                    await client.promote_chat_member(
+                        message.chat.id,
+                        user_id,
+                        can_change_info=True,
+                        can_delete_messages=True,
+                        can_restrict_members=True,
+                        can_invite_users=True,
+                        can_pin_messages=True,
+                    )
+
+                    await asyncio.sleep(2)
+                    await client.set_administrator_title(
+                        message.chat.id, user_id, custom_rank
+                  )
+                    await message.edit_text("promoted due to bribe")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                except UsernameInvalid:
+                    await message.edit_text("user_invalid"))
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+                except PeerIdInvalid:
+                    await message.edit_text("peer_invalid"))
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+                except UserIdInvalid:
+                    await message.edit_text("id_invalid"))
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+
+                except ChatAdminRequired:
+                    await message.edit_text("denied_permission"))
+                    await asyncio.sleep(5)
+                    await message.delete()
+                    return
+
+                except Exception as e:
+                    await message.edit_text(f"**Log:** `{e}`")
+                    return
+
+        else:
+            await message.edit_text("denied_permission"))
+            await asyncio.sleep(5)
+            await message.delete()
+    else:
+        await message.delete()
