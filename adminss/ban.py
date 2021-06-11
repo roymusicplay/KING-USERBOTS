855ec4d18b8b0e9a7fd2a1cfe0414b4648fc6f6d
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-11 16:39:10 +0530

    Update ban.py

--- adminss/ban.py
+++ adminss/ban.py
@@ -11,39 +11,51 @@ __**This command helps you to instantly ban a user in the chat**__
 """
 
 @kingbot.on_message(filters.group & filters.command("ban",vr.get("HNDLR")) & filters.user(Adminsettings))  
-async def member_ban(_ , message):
-    msg_id=message.message_id
-    chat_id=message.chat.id
-    zuz=await kingbot.get_chat_member(chat_id , "me")
-    can_ban=zuz.can_restrict_members
-    chat_msg=message.text
-    
-    user_id=None
-    if "@" in chat_msg:
-        index=chat_msg.index("@")     
-        chat_msg=str(chat_msg)
-        user_id=chat_msg[index+1:len(chat_msg)]
-    else:                   
-        user_id=message.reply_to_message.from_user.id
-    user_info=await kingbot.get_users(user_id)
-    zuze=await kingbot.get_chat_member(chat_id , user_id)
-    can_user_ban=zuze.can_restrict_members
+async def member_ban(client , message):
+    if message.chat.type in ["group", "supergroup"]:
+        chat_id = message.chat.id
+        me_m = client.me
+        me_ = await message.chat.get_member(int(me_m.id))
+        if not me_.can_restrict_members:
+         await message.edit("`You Don't Have Ban Permission!`")
+         return
+        can_ban= True
+        if can_ban:
+            try:
+                if message.reply_to_message:
+                    user_id = message.reply_to_message.from_user.id
+                else:
+                    usr = await client.get_users(message.command[1])
+                    user_id = usr.id
+            except IndexError:
+                await message.edit_text("I cant ban a void xD")
+                return
+            if user_id:
+                try:
+                    await client.kick_chat_member(chat_id, user_id)
+                    await message.delete()
+                except UsernameInvalid:
+                    await message.edit_text("`invalid username`")
+                    return
 
-    if(can_ban):
-        if(can_user_ban):
-            reply_string="Can't kick another admin. LOL !"
-            await kingbot.edit_message_text(chat_id , msg_id , reply_string )
-        else:            
-            bwll=await kingbot.kick_chat_member(chat_id , user_id)
-            if(user_info.username):
-                usercontact=user_info.username
-                reply_string="@"+usercontact+" has been kicked to hell 😈"
-                await kingbot.edit_message_text(chat_id , msg_id , reply_string)
-            else:
-                usercontact=user_info.first_name
-                reply_string=usercontact+" has been kicked to hell 😈"
-                await kingbot.edit_message_text(chat_id , msg_id , reply_string)
+                except PeerIdInvalid:
+                    await message.edit_text("`invalid username or userid`")
+                    return
+
+                except UserIdInvalid:
+                    await message.edit_text("`invalid userid`")
+                    return
+
+                except ChatAdminRequired:
+                    await message.edit_text("`permission denied`")
+                    return
+
+                except Exception as e:
+                    await message.edit_text(f"**Log:** `{e}`")
+                    return
+
+        else:
+            await message.edit_text("`permission denied`")
+            return
     else:
-        reply_string="Noob,you can't kick members 😂 !"
-        await kingbot.edit_message_text(chat_id , msg_id , reply_string )
-
+        await message.delete()
