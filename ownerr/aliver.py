6e91d11197c52d7414c3bb47feddb5781d35b632
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2021-06-16 06:51:30 +0530

    Update aliver.py

--- ownerr/aliver.py
+++ ownerr/aliver.py
@@ -9,7 +9,7 @@ __**This command helps you to check wether userbot is alive**__
 ──「 **Usage** 」──
 -> `alive`
 """
-Alive_pic = "https://telegra.ph/file/2ab60ae1ddc6cd3a0f97b.jpg"
+Alive_pic = "https://telegra.ph/file/664686c15a83ccf26239b.mp4"
 @kingbot.on_message(filters.user(Adminsettings) & filters.command("alive",vr.get("HNDLR")))
 async def gooe_search(client, message):
     start_time = time.time()
@@ -21,7 +21,7 @@ async def gooe_search(client, message):
     reply_msg += f"🔸Pɪɴɢ Tɪᴍᴇ: **{ping_time}ms\n**"
     reply_msg += f"🔹Kɪɴɢ Uᴘᴛɪᴍᴇ: **{uptime}\n**"
     reply_msg += f"🔸Sᴜᴘᴘᴏʀᴛ: **@KingUserBots\n**"
-    reply_msg += f"🔹Fᴏʀᴋ ʙʏ: **@DeCoDeRoBoT\n**"
+    reply_msg += f"🔹Rᴇᴘᴏ: **[HERE](https://github.com/ToxicCybers/kinguserbot\n**"
     reply_msg += f"🔸Pʏᴛʜᴏɴ: **3.8\n\n**"
     reply_msg += f"🍹Sᴇʀᴠᴇʀꜱ Fᴜɴᴄᴛɪᴏɴɪɴɢ Nᴏʀᴍᴀʟ🍹"
     await client.send_photo(message.chat.id , Alive_pic , reply_msg)
